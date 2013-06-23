a9509fda4610783a261370863490f344802dee5e
linuxknow@gmail.com <linuxknow@gmail.com>
2013-06-23 19:27:03 -0300

    subiendo los 100 numeros primos

--- node.js
+++ node.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
+
+//Copyright (C) 2013  linuxknow@gmail.com
+
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 var fs = require('fs');
 var outfile = "hello.txt";
 var out = "A startup is a business built to grow rapidly, This is an example for app node.js.\n";
